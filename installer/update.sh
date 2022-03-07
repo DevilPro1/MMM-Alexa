5592164ed6728d930ac735923c0cd8ace514a2d6
devilpro1 <92226222+DevilPro1@users.noreply.github.com>
2022-03-07 20:16:34 +0100

    Add files via upload

---
+++ installer/update.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+# +---------+
+# | updater |
+# +---------+
+
+# get the installer directory
+Installer_get_current_dir () {
+  SOURCE="${BASH_SOURCE[0]}"
+  while [ -h "$SOURCE" ]; do
+    DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+    SOURCE="$(readlink "$SOURCE")"
+    [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE"
+  done
+  echo "$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+}
+
+Installer_dir="$(Installer_get_current_dir)"
+
+# move to installler directory
+cd "$Installer_dir"
+source utils.sh
+if $prompt; then
+  Installer_info "Welcome to Alexa updater !"
+  echo
+fi
+
+cd ~/MagicMirror/modules/MMM-Alexa
+# deleting package.json because npm install add/update package
+rm -f package.json package-lock.json
+
+Installer_info "Updating..."
+
+
+git reset --hard HEAD
+git pull
+#fresh package.json
+git checkout package.json
+cd ~/MagicMirror/modules/MMM-Alexa/node_modules
+
+Installer_info "Deleting ALL @devilpro1 libraries..."
+
+rm -rf @devilpro1
+cd ~/MagicMirror/modules/MMM-Alexa
+
+Installer_info "Ready for Updating..."
+
+# launch installer
+npm install
